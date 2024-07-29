2dc8f2a585405a8184f6db03eb1dd7ec93520872
Shwetaa79 <44580521+Shwetaa79@users.noreply.github.com>
2024-07-29 17:24:04 +0530

    Create App.js

---
+++ Assignment3/src/App.js
@@ -0,0 +1,12 @@
+import Gallery from './components/Gallery';
+import './scripts/scripts.css'
+
+function App() {
+  return (
+    <div className="App">
+        <Gallery />
+    </div>
+  );
+}
+
+export default App;
